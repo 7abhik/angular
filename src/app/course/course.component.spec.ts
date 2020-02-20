3c886b7ff1821d48e8190cfbdcfcb68f5fd372bb
abhi <abhishek.kumar@klugkraft.com>
2020-02-20 10:49:42 +0530

    pulling latest code

--- src/app/course/course.component.spec.ts
+++ src/app/course/course.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeroesComponent } from './heroes.component';
+import { CourseComponent } from './course.component';
 
-describe('HeroesComponent', () => {
-  let component: HeroesComponent;
-  let fixture: ComponentFixture<HeroesComponent>;
+describe('CourseComponent', () => {
+  let component: CourseComponent;
+  let fixture: ComponentFixture<CourseComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [ CourseComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeroesComponent);
+    fixture = TestBed.createComponent(CourseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/courses.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'courses',
+    template:'<h1> {{ getTitle() }} </h1>'
+})
+
+export class CoursesComponent {
+    title = "List Of courses";
+    getTitle(){
+        return this.title;
+    }
+}
